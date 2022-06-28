ad613790d7a9f9156cfa2f8cf1fb12ec56ab4b8f
AbuBakrKhan24 <abubakrkhan224@gmail.com>
2022-06-28 16:29:37 +0200

    first commit

---
+++ public/index.js
@@ -0,0 +1,43 @@
+// localStorage.removeItem('records');
+// Adding array
+
+// JSON.parse(localStorage.getItem("Stored_Info"))
+//   ? JSON.parse(localStorage.getItem("Stored_Info"))
+//   :
+
+let items = [
+  {
+    id: 1,
+    product: "TV Stand",
+    createDate: new Date(),
+  },
+];
+
+// Original Loop
+function showItems(items) {
+  document.querySelector("#list").innerHTML = "";
+  items.forEach((item) => {
+    console.log(item);
+    document.getElementById("list").innerHTML += `
+    <div class="item" id="listing"><input class="chb checkboxes"  type="checkbox"  ><label for="add_info">${item.product}</label><i class="fa-solid fa-trash-can"></i></div>
+    `;
+  });
+}
+showItems(items);
+
+// Add Function
+function add() {
+  NewItem = {
+    id: items.length + 1,
+    product: document.getElementById("add_info").value,
+    createDate: new Date(),
+  };
+  items.push(NewItem);
+  document.getElementById("list").innerHTML += `
+  <div class="item" id="listing"><input class="chb checkboxes " type="checkbox"><label for="add_info">${NewItem.product}</label></div>
+  `;
+
+  console.log(items);
+  //   localStorage.setItem("Stored_Info", JSON.stringify(items));
+  showItems(items);
+}
